11f5987ccda37b7781b9fa506ffa7c8183cc8d46
“hanol2” <hsring23@gmail.com>
2024-01-10 15:53:52 +0900

    🔐 chore: .env gitignore 추가

--- src/firebaseApp.ts
+++ src/firebaseApp.ts
@@ -8,24 +8,14 @@ export let app : FirebaseApp;
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// const firebaseConfig = {
-//   apiKey:  process.env.REACT_APP_API_KEY,
-//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-//   projectId: process.env.REACT_APP_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-//   messagingSenderId:process.env.REACT_APP_MESSAGING_SENTER_ID,
-//   appId: process.env.REACT_APP_ID
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyDUHzrSeW1-T5D-aY3UNRelovpQ7gAgb5Q",
-  authDomain: "fastcampus-react-blog-2e225.firebaseapp.com",
-  projectId: "fastcampus-react-blog-2e225",
-  storageBucket: "fastcampus-react-blog-2e225.appspot.com",
-  messagingSenderId: "468882163317",
-  appId: "1:468882163317:web:83743b952bfd84a2605a3c"
+  apiKey:  process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId:process.env.REACT_APP_MESSAGING_SENTER_ID,
+  appId: process.env.REACT_APP_ID
 };
-
 
 try {
   app = getApp("app")
